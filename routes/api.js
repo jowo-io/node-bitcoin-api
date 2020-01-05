74600ab548abc70ff85d9e975c34f9f7edd2ac3d
JoWo <pureth@googlemail.com>
2020-01-05 08:10:22 +0100

    adding various api methods
    when invoked, these methods correspond/interface
    with the RPC api. In the tutorial it was recommended to copy and paste the methods 8 times. I feel like a simple forEach loop is more appropriate here.

--- routes/api.js
+++ routes/api.js
@@ -12,8 +12,44 @@ const headers = {
     "content-type": "text/plain;"
 };
 
+const methodsList = [
+    "getblockcount",
+    "getbestblockhash",
+    "getconnectioncount",
+    "getdifficulty",
+    "getblockchaininfo",
+    "getmininginfo",
+    "getpeerinfo",
+    "getrawmempool"
+];
+
 router.get("/test", (req, res) => res.json({ msg: "backend works" }));
 
-// ... routes will go here
+methodsList.forEach(method => {
+    router.get(`/${method}`, (req, res) => {
+        const body = JSON.stringify({
+            jsonrpc: "1.0",
+            id: "curltext",
+            method,
+            params: []
+        });
+
+        const options = {
+            url: `http://${USER}:${PASS}@127.0.0.1:8332/`,
+            method: "POST",
+            headers,
+            body
+        };
+
+        function callback(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                const data = JSON.parse(body);
+                res.send(data);
+            }
+        }
+
+        request(options, callback);
+    });
+});
 
 module.exports = router;
